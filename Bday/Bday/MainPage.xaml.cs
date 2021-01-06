9932f4ed391adcce3f8a73268afe975e47114e5c
алекс <@DESKTOP-TVQ711I>
2021-01-06 10:53:42 +0200

    Добавьте файлы проекта.

---
+++ Bday/Bday/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Messaging;
+
+namespace Bday
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var smsMessenger = CrossMessaging.Current.SmsMessenger;
+            if (smsMessenger.CanSendSms)
+                smsMessenger.SendSms(phNum.Text, Holiday.SelectedItem);
+        }
+    }
+}
